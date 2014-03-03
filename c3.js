0391968b12d7aad727eeb3e77c32801ae416a50b
Masayuki Tanaka <masayuki0812@mac.com>
2014-03-03 19:29:11 +0900

    Fix value on chart with line chart

--- c3.js
+++ c3.js
@@ -1079,7 +1079,6 @@
         }
         function getBarX(barW, barTargetsNum, barIndices, isSub) {
             var scale = isSub ? subX : x;
-            if (! barTargetsNum) { return function () { return 0; }; }
             return function (d) {
                 var barIndex = d.id in barIndices ? barIndices[d.id] : 0;
                 return d.x || d.x === 0 ? scale(d.x) - barW * (barTargetsNum / 2 - barIndex) : 0;
