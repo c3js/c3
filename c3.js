fef7457987ae3901a8b85d18febdf925b7421d7d
Jason Xun Xu <dev@jasonxu.net>
2014-05-24 20:06:20 -0700

    Fixed using numeric values as category names.

--- c3.js
+++ c3.js
@@ -4897,8 +4897,7 @@
             }
             return newScale;
         }
-        function textFormatted(i) {
-            var v = isCategory && i < categories.length ? categories[i] : i;
+        function textFormatted(v) {
             return tickFormat ? tickFormat(v) : v;
         }
         function axis(g) {
