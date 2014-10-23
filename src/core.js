9797f2ec073fae167807d27839c7d3dbad4bded9
nqounet <mail@nqou.net>
2014-10-23 11:07:41 +0900

    fixed error caused by used with ExtJS

--- src/core.js
+++ src/core.js
@@ -10,6 +10,7 @@ function Chart(config) {
     // bind "this" to nested API
     (function bindThis(fn, target, argThis) {
         for (var key in fn) {
+            if (key === '$extIsFunction') { continue; }
             target[key] = fn[key].bind(argThis);
             if (Object.keys(fn[key]).length > 0) {
                 bindThis(fn[key], target[key], argThis);
