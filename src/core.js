5d31edfa17662025392a6997e485ac2a93e2ed39
Tropicalista <francescopepe@email.it>
2014-11-20 15:28:55 +0100

    Update core.js

--- src/core.js
+++ src/core.js
@@ -109,7 +109,8 @@ c3_chart_internal_fn.initParams = function () {
 
     $$.xOrient = config.axis_rotated ? "left" : "bottom";
     $$.yOrient = config.axis_rotated ? "bottom" : "left";
-    $$.y2Orient = config.axis_rotated ? "top" : "right";
+    $$.yOrient = config.axis_rotated ? (config.axis_y_inner ? "top" : "bottom") : (config.xis_y_inner ? "right" : "left");
+    $$.y2Orient = config.axis_rotated ? (config.axis_y_inner ? "bottom" : "top") : (config.xis_y_inner ? "left" : "right");
     $$.subXOrient = config.axis_rotated ? "left" : "bottom";
 
     $$.isLegendRight = config.legend_position === 'right';
