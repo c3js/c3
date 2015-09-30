7caa94645667a619227341b6fdc6580b4d52c9fa
Partha Pal <parthapal33@gmail.com>
2015-09-30 12:34:39 +0530

    Added api axis.cullingMax

--- src/api.axis.js
+++ src/api.axis.js
@@ -58,3 +58,21 @@ c3_chart_fn.axis.range = function (range) {
         };
     }
 };
+
+c3_chart_fn.axis.cullingMax = function(max){
+    var $$ = this.internal, config = $$.config;
+	if(arguments.length){
+		if(isValue(max)){
+			config.axis_x_tick_culling_max = max;
+			$$.redraw({
+				withUpdateXAxis:true,            
+                withY: false,
+                withSubchart: false,
+                withEventRect: false,
+                withTransitionForAxis: false
+			});
+		}
+	}else{
+		return config.axis_x_tick_culling_max;
+	}
+};
