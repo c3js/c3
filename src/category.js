3829ec9044591e40b8334a8837699cbf723818c7
Dekai Zhang <dekai.zhang@mastercard.com>
2016-04-01 17:41:01 +0800

    Fixes some tick of categories are missing to display

--- src/category.js
+++ src/category.js
@@ -1,4 +1,4 @@
 c3_chart_internal_fn.categoryName = function (i) {
-    var config = this.config;
-    return i < config.axis_x_categories.length ? config.axis_x_categories[i] : i;
+    var config = this.config, categoryIndex = Math.ceil(i);
+    return i < config.axis_x_categories.length ? config.axis_x_categories[categoryIndex] : i;
 };
