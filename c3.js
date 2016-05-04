223404a79246c61d150d8a209dbdc81f6955083b
Masayuki Tanaka <masayuki0812@mac.com>
2016-05-04 10:37:19 +0900

    Update version to 0.4.11

--- c3.js
+++ c3.js
@@ -3,7 +3,7 @@
 
     /*global define, module, exports, require */
 
-    var c3 = { version: "0.4.11-rc4" };
+    var c3 = { version: "0.4.11" };
 
     var c3_chart_fn,
         c3_chart_internal_fn,
--- src/core.js
+++ src/core.js
@@ -1,4 +1,4 @@
-var c3 = { version: "0.4.11-rc4" };
+var c3 = { version: "0.4.11" };
 
 var c3_chart_fn,
     c3_chart_internal_fn,
