f7dbd4d195cbb1960c1fbd538052f295974b920a
Yoshiya Hinosawa <stibium121@gmail.com>
2017-06-03 12:33:37 +0900

    fix: fix test case of mouse events

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,12 @@
+const path = require('path');
+
 module.exports = function(config) {
   config.set({
     frameworks: ['jasmine', 'browserify'],
     files: [
-      'node_modules/d3/d3.min.js',
       'c3.css',
       'spec/*-helper.js',
-      'spec/*-spec.js',
+      'spec/*-spec.js'
     ],
     preprocessors: {
       'spec/c3-helper.js': ['browserify']
