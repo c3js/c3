92977d59b5c94eb45ab0d9b368631f9f422182ad
Masayuki Tanaka <masayuki0812@mac.com>
2018-01-08 22:19:14 +0900

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-    entry: 'src/index.js',
-    format: 'umd',
-    moduleName: 'c3',
+    input: 'src/index.js',
+    output: {
+        format: 'umd',
+        name: 'c3',
+    },
     plugins: [babel({
         presets: [['es2015', {
             modules: false
