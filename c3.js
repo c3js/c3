b69f938acb96eb646e42da4cb14057b1c43ff939
Yoshiya Hinosawa <stibium121@gmail.com>
2018-05-14 22:35:50 +0900

    chore(version): bump to v0.6.0

--- c3.js
+++ c3.js
@@ -1,4 +1,4 @@
-/* @license C3.js v0.5.4 | (c) C3 Team and other contributors | http://c3js.org/ */
+/* @license C3.js v0.6.0 | (c) C3 Team and other contributors | http://c3js.org/ */
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
@@ -996,7 +996,7 @@
         $$.axes.subx.style("opacity", isHidden ? 0 : 1).call($$.subXAxis, transition);
     };
 
-    var c3 = { version: "0.5.4" };
+    var c3 = { version: "0.6.0" };
 
     var c3_chart_fn;
     var c3_chart_internal_fn;
