7a2aa504320a5b914f72fa66e2c7005cbf1d7bf6
Joao Morais <jcsmorais@gmail.com>
2018-05-15 10:43:32 -0700

    Add String.padEnd polyfill

--- src/polyfill.js
+++ src/polyfill.js
@@ -871,4 +871,25 @@ if (!Function.prototype.bind) {
     }
 }());
 
+// String.padEnd polyfill for IE11
+//
+// https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padEnd
+if (!String.prototype.padEnd) {
+    String.prototype.padEnd = function padEnd(targetLength,padString) {
+        targetLength = targetLength>>0; //floor if number or convert non-number to 0;
+        padString = String((typeof padString !== 'undefined' ? padString : ' '));
+        if (this.length > targetLength) {
+            return String(this);
+        }
+        else {
+            targetLength = targetLength-this.length;
+            if (targetLength > padString.length) {
+                padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+            }
+            return String(this) + padString.slice(0,targetLength);
+        }
+    };
+}
+
 /* jshint ignore:end */
