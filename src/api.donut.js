ae8c3424661fb6e60989ad4ec9b34eb216bca331
Anthony Pessy <anthony@cogniteev.com>
2019-08-07 11:42:13 +0200

    add API to get/set padAngle for both pie and donut

---
+++ src/api.donut.js
@@ -0,0 +1,11 @@
+import { Chart } from './core';
+
+Chart.prototype.donut = function() {};
+
+Chart.prototype.donut.padAngle = function (padAngle) {
+    if (padAngle === undefined) {
+        return this.internal.config.donut_padAngle;
+    }
+    this.internal.config.donut_padAngle = padAngle;
+    this.flush();
+};
