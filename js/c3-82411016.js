914368a95298fab93550b39cfe456e74d8932350
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-20 07:08:28 +0000

    Deploy c3js/c3 to github.com/c3js/c3.git:gh-pages

--- js/c3-82411016.js
+++ js/c3-82411016.js
@@ -1,4 +1,4 @@
-/* @license C3.js v0.7.5 | (c) C3 Team and other contributors | http://c3js.org/ */
+/* @license C3.js v0.7.6 | (c) C3 Team and other contributors | http://c3js.org/ */
 
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
@@ -1200,7 +1200,7 @@
   };
 
   var c3 = {
-    version: "0.7.5",
+    version: "0.7.6",
     chart: {
       fn: Chart.prototype,
       internal: {
@@ -7049,7 +7049,7 @@
       return d.value;
     }).reduce(function (p, c) {
       return p + c;
-    });
+    }, 0);
     $$.addToCache('getTotalDataSum', totalDataSum);
     return totalDataSum;
   };
